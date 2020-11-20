7ea395a5dba9a7fa6ed38e006c8d79dcea1f49e4
kimyang-sun <berman532@gmail.com>
2020-11-20 17:56:07 +0900

    app 컴포넌트 생성

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,46 @@
+import React, { Component } from "react";
 import "./app.css";
 import Items from "./components/items";
 
-function App() {
-  return <Items></Items>;
+class App extends Component {
+  state = {
+    items: [
+      { id: 1, name: "item 1", count: 0 },
+      { id: 2, name: "item 2", count: 0 },
+      { id: 3, name: "item 3", count: 0 },
+    ],
+  };
+
+  handleIncrement = item => {
+    const items = [...this.state.items];
+    const index = items.indexOf(item);
+    items[index].count++;
+    this.setState({ items });
+  };
+
+  handleDecrement = item => {
+    const items = [...this.state.items];
+    const index = items.indexOf(item);
+    const count = items[index].count - 1;
+    items[index].count = count < 0 ? 0 : count;
+    this.setState({ items });
+  };
+
+  handleDelete = item => {
+    const items = this.state.items.filter(_item => _item.id !== item.id);
+    this.setState({ items });
+  };
+
+  render() {
+    return (
+      <Items
+        items={this.state.items}
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+        onDelete={this.handleDelete}
+      ></Items>
+    );
+  }
 }
 
 export default App;
--- src/components/items.jsx
+++ src/components/items.jsx
@@ -2,38 +2,22 @@ import React, { Component } from "react";
 import Item from "./item";
 
 export default class Items extends Component {
-  state = {
-    items: [
-      { id: 1, name: "item 1", count: 0 },
-      { id: 2, name: "item 2", count: 0 },
-      { id: 3, name: "item 3", count: 0 },
-    ],
-  };
-
   handleIncrement = item => {
-    const items = [...this.state.items];
-    const index = items.indexOf(item);
-    items[index].count++;
-    this.setState({ items });
+    this.props.onIncrement(item);
   };
 
   handleDecrement = item => {
-    const items = [...this.state.items];
-    const index = items.indexOf(item);
-    const count = items[index].count - 1;
-    items[index].count = count < 0 ? 0 : count;
-    this.setState({ items });
+    this.props.onDecrement(item);
   };
 
   handleDelete = item => {
-    const items = this.state.items.filter(_item => _item.id !== item.id);
-    this.setState({ items });
+    this.props.onDelete(item);
   };
 
   render() {
     return (
       <ul>
-        {this.state.items.map(item => (
+        {this.props.items.map(item => (
           <Item
             key={item.id}
             item={item}
